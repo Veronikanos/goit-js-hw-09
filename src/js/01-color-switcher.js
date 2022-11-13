32366f5af6897722e9fdeb82ce5141645b2b1f64
Veronica <vveronikanika@gmail.com>
2022-11-13 17:08:04 +0200

    feat: 1 task

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,30 @@
+const buttonStart = document.querySelector('[data-start]');
+const buttonStop = document.querySelector('[data-stop]');
+const bodyEl = document.querySelector('body');
+let timerId = null;
 
+buttonStop.disabled = true;
+
+buttonStart.addEventListener('click', startChangeColor);
+buttonStop.addEventListener('click', stopChangeColor);
+
+function startChangeColor(event) {
+  timerId = setInterval(callback, 1000);
+
+  buttonStop.disabled = !buttonStop.disabled;
+  buttonStart.disabled = !buttonStart.disabled;
+}
+
+function stopChangeColor(event) {
+  buttonStop.disabled = !buttonStop.disabled;
+  buttonStart.disabled = !buttonStart.disabled;
+  clearInterval(timerId);
+}
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+
+function callback() {
+  bodyEl.style.backgroundColor = getRandomHexColor();
+}
