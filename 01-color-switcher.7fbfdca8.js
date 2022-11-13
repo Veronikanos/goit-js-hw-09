682ebcacd43d8e336579d784e8628b816d951e1f
Veronikanos <49239848+Veronikanos@users.noreply.github.com>
2022-11-13 15:08:25 +0000

    Deploying to gh-pages from @ Veronikanos/goit-js-hw-09@32366f5af6897722e9fdeb82ce5141645b2b1f64 🚀

---
+++ 01-color-switcher.7fbfdca8.js
@@ -0,0 +1,2 @@
+const e=document.querySelector("[data-start]"),d=document.querySelector("[data-stop]"),t=document.querySelector("body");let a=null;function l(){t.style.backgroundColor=`#${Math.floor(16777215*Math.random()).toString(16)}`}d.disabled=!0,e.addEventListener("click",(function(t){a=setInterval(l,1e3),d.disabled=!d.disabled,e.disabled=!e.disabled})),d.addEventListener("click",(function(t){d.disabled=!d.disabled,e.disabled=!e.disabled,clearInterval(a)}));
+//# sourceMappingURL=01-color-switcher.7fbfdca8.js.map
